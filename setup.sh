cb60c6354437df7c6c70d3edc4c7d42d13cefda1
David Abdurachmanov <david.abdurachmanov@sifive.com>
2020-04-01 07:23:57 -0700

    setup.sh: add vulkan to DISTRO_FEATURES
    Signed-off-by: David Abdurachmanov <david.abdurachmanov@sifive.com>

--- setup.sh
+++ setup.sh
@@ -83,7 +83,7 @@ require conf/distro/include/security_flags.inc
 
 INHERIT += "uninative"
 
-DISTRO_FEATURES_append = " largefile opengl ptest multiarch wayland pam  systemd "
+DISTRO_FEATURES_append = " largefile opengl ptest multiarch wayland pam systemd vulkan "
 DISTRO_FEATURES_BACKFILL_CONSIDERED += "sysvinit"
 VIRTUAL-RUNTIME_init_manager = "systemd"
 HOSTTOOLS_NONFATAL_append = " ssh"
